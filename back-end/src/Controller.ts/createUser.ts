d9f8f378dff1299cf7b0d914413e9393803dbd23
Guilherme Henrique <guihenriquesilva10@gmail.com>
2025-01-06 18:58:36 -0300

    Controller-CreateUser

---
+++ back-end/src/Controller.ts/createUser.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+
+import createUserService from "../services/createUserService";
+
+
+export default async function createUser(req: Request, res: Response): Promise<void> {
+    const userData = req.body;
+  
+    try {
+      await createUserService.execute(userData);
+      console.log('Usuário criado com sucesso');
+      res.status(201).json({ message: 'Usuário criado com sucesso!' });
+    } catch (error) {
+      console.error('Erro ao criar usuário:', error);
+      res.status(500).json({ message: 'Erro ao criar usuário', error });
+    }
+  }
