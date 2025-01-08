77b6a77a09a0ef532ffa28488db842e59576b610
Guilherme Henrique <guihenriquesilva10@gmail.com>
2025-01-07 21:29:19 -0300

    changes

--- front-end/src/cadastro/cadastro.tsx
+++ front-end/src/cadastro/cadastro.tsx
@@ -10,38 +10,40 @@ export default function Cadastro() {
               Cadastro
             </h1>
           </div>
-          <div className="dados-formulario">
-            <label htmlFor="nome" className="form_label" id="label">Nome</label>
-            <input
-              type="text"
-              name="nome"
-              className="form_input"
-              id="nome"
-              placeholder="Nome"
-              required
-            />
-          </div>
-          <div className="dados-formulario">
-            <label htmlFor="Email" className="form_label" id="label">Email</label>
-            <input
-              type="email"
-              name="Email"
-              className="form_input"
-              id="Email"
-              placeholder="Email"
-              required
-            />
-          </div>
-          <div className="dados-formulario">
-            <label htmlFor="Password" className="form_label" id="label">Password</label>
-            <input
-              type="password"
-              name="Password"
-              className="form_input"
-              id="Password"
-              placeholder="Password"
-              required
-            />
+          <div className="main-form">
+            <div className="dados-formulario">
+              <label htmlFor="nome" className="form_label" id="label">Nome</label>
+              <input
+                type="text"
+                name="nome"
+                className="form_input"
+                id="nome"
+                placeholder="Nome"
+                required
+              />
+            </div>
+            <div className="dados-formulario">
+              <label htmlFor="Email" className="form_label" id="label">Email</label>
+              <input
+                type="email"
+                name="Email"
+                className="form_input"
+                id="Email"
+                placeholder="Email"
+                required
+              />
+            </div>
+            <div className="dados-formulario">
+              <label htmlFor="Password" className="form_label" id="label">Password</label>
+              <input
+                type="password"
+                name="Password"
+                className="form_input"
+                id="Password"
+                placeholder="Password"
+                required
+              />
+            </div>
           </div>
         </div>
       </div>
