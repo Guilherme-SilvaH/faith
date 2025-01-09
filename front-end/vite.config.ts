934cc05aa6c6bbd97094758375c02a968b33fb45
Guilherme Silva <guihenriquesilva10@gmail.com>
2025-01-09 08:15:38 -0300

    Update vite.config.ts

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Certifique-se de que a saída seja para o diretório correto
+  },
+  server: {
+    port: 3000,
+  },
+  base: './', // Necessário para deploy relativo
+});
+
