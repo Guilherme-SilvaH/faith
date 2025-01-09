b80a808085d580bf3f45675fd8f64df917f2d158
Guilherme Henrique <guihenriquesilva10@gmail.com>
2025-01-09 20:06:24 -0300

    cors

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -7,7 +7,7 @@ const app = express();
 
 // Habilitar CORS antes das rotas
 app.use(cors({
-  origin: 'http://localhost:5173',  // Permite apenas a origem do seu front-end
+  origin: 'https://my-bible-reader.vercel.app/',  // Permite apenas a origem do seu front-end
   methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Define os métodos permitidos
   allowedHeaders: ['Content-Type', 'Authorization'],  // Permite cabeçalhos necessários
 }));
