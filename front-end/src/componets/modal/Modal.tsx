bbdef5807fd8aa1d4adca94fadf414f4e9eb737b
Guilherme Silva <guihenriquesilva10@gmail.com>
2025-02-06 08:48:04 -0300

    Update Modal.tsx

--- front-end/src/componets/modal/Modal.tsx
+++ front-end/src/componets/modal/Modal.tsx
@@ -1,8 +1,8 @@
 
+// MyVerticallyCenteredModal.tsx
 import Button from 'react-bootstrap/Button';
 import Modal, { ModalProps } from 'react-bootstrap/Modal';
-import "./Modal.sass"
-
+import styles from "./Modal.module.sass"; // agora usando CSS Modules
 
 interface MyModalProps extends ModalProps {}
 
@@ -13,14 +13,17 @@ export default function MyVerticallyCenteredModal(props: MyModalProps) {
       size="lg"
       aria-labelledby="contained-modal-title-vcenter"
       centered
+      dialogClassName={styles.customModal} // Usando a classe do CSS Module
     >
-    <Modal.Title>
-      Faith
-    </Modal.Title>
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Faith
+        </Modal.Title>
+      </Modal.Header>
       <Modal.Body>
-            <p>
-            Faith é uma aplicação web criada para ajudar no acompanhamento da leitura diária da Bíblia. O projeto nasceu de uma necessidade pessoal, onde eu e minha namorada procurávamos um lugar para registrar e organizar nossas leituras diárias. Assim, surgiu a ideia de criar este site.
-            </p>
+        <p>
+          Faith é uma aplicação web criada para ajudar no acompanhamento da leitura diária da Bíblia. O projeto nasceu de uma necessidade pessoal, onde eu e minha namorada procurávamos um lugar para registrar e organizar nossas leituras diárias. Assim, surgiu a ideia de criar este site.
+        </p>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
